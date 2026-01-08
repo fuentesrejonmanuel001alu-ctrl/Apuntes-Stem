40ca3c62b7a3afaeef3372cd96e1370090c06aa8
fuentesrejonmanuel001.alu@stemgranada.com <fuentesrejonmanuel001.alu@stemgranada.com>
2026-01-08 09:14:32 +0100

    actualizado

---
+++ PROGRAMACION/JAVA/CocheColor.java
@@ -0,0 +1,10 @@
+public class CocheColor {
+    public static void main(String[] args) {
+        // 1. Elegimos el color (Cámbialo si quieres)
+        String miColor = "Verde Esmeralda";
+        String miMarca = "Ferrari"
+        // 2. Le decimos a Java que nos lo diga
+        System.out.println("Mi coche es de color: " + miColor);
+        System.out.println("Y es un: " + miMarca);
+    }
+}
