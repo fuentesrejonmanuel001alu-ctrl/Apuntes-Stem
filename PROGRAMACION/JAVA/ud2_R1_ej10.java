ba14fa10fcf744deca3ed9d7f935bdf39d75ad7d
fuentesrejonmanuel001.alu@stemgranada.com <fuentesrejonmanuel001.alu@stemgranada.com>
2026-01-22 13:56:17 +0100

    actualizo

---
+++ PROGRAMACION/JAVA/ud2_R1_ej10.java
@@ -0,0 +1,14 @@
+package PROGRAMACION.JAVA;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ud2_R1_ej10 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> cola = new ArrayList<>(Arrays.asList());
+        cola.add(10, 20, 30);
+
+    }
+
+}
