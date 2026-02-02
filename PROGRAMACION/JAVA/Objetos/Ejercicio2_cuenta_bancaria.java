6d04d4f1dd036a7eb453166fc36f9da43ccd85fa
fuentesrejonmanuel001.alu@stemgranada.com <fuentesrejonmanuel001.alu@stemgranada.com>
2026-02-02 09:55:45 +0100

    Ejercicios objetos java

---
+++ PROGRAMACION/JAVA/Objetos/Ejercicio2_cuenta_bancaria.java
@@ -0,0 +1,21 @@
+package Objetos;
+
+public class Ejercicio2_cuenta_bancaria {
+
+    public static void main(String[] args) {
+                
+        class cuentaBancaria ()
+        String titular;
+        double saldo;
+        double depositarCantidad(){
+            this.saldo>=0;
+        }
+        double retirar(cantidad){
+            this.retirar()
+        }
+        cuentaBancaria(String titular, double saldo){
+            
+        }
+    }
+
+}
