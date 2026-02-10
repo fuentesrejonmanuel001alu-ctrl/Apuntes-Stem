e2c3a32b8a66102f9475a837347a90249cc46106
fuentesrejonmanuel001.alu@stemgranada.com <fuentesrejonmanuel001.alu@stemgranada.com>
2026-02-10 12:05:36 +0100

    hecho

---
+++ PROGRAMACION/JAVA/Objetos/Ejercicio_clase_get_set.java
@@ -0,0 +1,13 @@
+package Objetos;
+
+public class Ejercicio_clase_get_set {
+
+    public static void main(String[] args) {
+        Estudiante estudiante1= new Estudiante();
+        estudiante1.nombre="Alonso";
+    }
+    
+}
+class Estudiante{
+    private 
+}
