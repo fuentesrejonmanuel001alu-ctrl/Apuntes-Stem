25cda31ab0ac4f303affc27b1db608951758134b
fuentesrejonmanuel001.alu@stemgranada.com <fuentesrejonmanuel001.alu@stemgranada.com>
2026-02-24 13:04:33 +0100

    programación java

--- PROGRAMACION/JAVA/practicando.java
+++ PROGRAMACION/JAVA/practicando.java
@@ -1,12 +1,33 @@
+import java.util.Scanner; // 1. Fundamental para leer el teclado
+
 public class practicando {
 
-     public static void main(String[] args) {
-        
-        System.out.println("Tu sueldo es: " + calcularSueldo(8, 10) + " €");
-     }
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in); // Creamos el escáner
 
-     public static int calcularSueldo(int horas, int pagoPorHora){
+      System.out.println("Dime una contraseña");
+      String entrada = sc.nextLine(); // Leemos lo que escribe el usuario
 
-         return horas * pagoPorHora;
-        }
+      // Llamamos a tu función y guardamos el resultado
+      boolean esValida = validar(entrada);
+
+      if (esValida) {
+         System.out.println("✅ Acceso permitido");
+      } else {
+         System.out.println("❌ Contraseña incorrecta");
+      }
+
+      sc.close();
+   } // <-- Aquí faltaba cerrar el main
+
+   public static boolean validar(String pass){
+              
+      if (pass.equals("Java123")){
+
+         return true;
+
+      } else {
+      return false;
+      }
+   }
 }
